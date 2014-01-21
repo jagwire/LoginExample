2b63f2c63fb14a9d4ceb1386b90cda3aa0c66731
JagWire <babiuchr@missouri.edu>
2014-01-21 12:11:05 -0600

    Up and running!

--- src/java/StartupListener.java
+++ src/java/StartupListener.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+
 /**
  * Web application lifecycle listener.
  *
--- src/java/login/example/ExampleApp.java
+++ src/java/login/example/ExampleApp.java
@@ -8,12 +8,21 @@ package login.example;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import login.resources.LoginResource;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ResourceFinder;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
 /**
  *
  * @author Ryan
  */
-@ApplicationPath("/resources")
-public class ExampleApp extends Application {
-
+@ApplicationPath("resources")
+public class ExampleApp extends ResourceConfig {
+    public ExampleApp() {
+        super();
+        packages("login.resources");
+        register(RolesAllowedDynamicFeature.class);
+    }
 
 }
